7e3c4e7afd094868df24cf65b1110b27fbe90403
gzstyp <444141300@qq.com>
2020-07-31 01:44:55 +0800

    添加.native属性表示是支持原生的方法,因为组件 el-dropdown 不提供click方法

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,7 @@
             <el-dropdown-menu slot="dropdown">
                 <el-dropdown-item icon="el-icon-plus">个人信息</el-dropdown-item>
                 <el-dropdown-item icon="el-icon-circle-plus">修改密码</el-dropdown-item>
-                <el-dropdown-item icon="el-icon-close" @click.native="logout()">退出登录</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-close" @click.native="logout()">退出登录</el-dropdown-item><!--添加.native属性表示是支持原生的方法,因为组件 el-dropdown 不提供click方法-->
             </el-dropdown-menu>
         </el-dropdown>
         <label :to="current.path">{{current.label || '首页'}}</label>
@@ -36,6 +36,7 @@
             },
             logout(){
                 this.$store.commit('clearMenu');
+                this.$store.commit('clearToken');
                 //必须注意这个name，否则vue会提示[vue-router] Route with name '/' does not exist !!!,被炕了N多次!!!
                 this.$router.push({name:'login'});//使用自定义通过name来进行跳转,所以需要给定一个name,采用的是编程式导航,进行页面跳转
             }
